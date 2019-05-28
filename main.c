95ed41eb0edcd01a1cdbc1c210fb2dcb808f563d
chelesgaroth <47673194+chelesgaroth@users.noreply.github.com>
2019-05-28 16:19:00 +0100

    Add files via upload

--- main.c
+++ main.c
@@ -1,16 +1,22 @@
 #include <stdio.h>
 
-int main() {
-    int x,med,i;
-    med=0;
-    i=0;
-    scanf ("%d",&x);
-    while (x !=0) {
-        med =  med + x;
-        i=i+1;
-        scanf ("%d",&x);
+
+int bitsUm (int n,int i) {
+    i=1;
+    while (n!=1 && n!=0) {
+        if (n%2 == 1) i=i+1;
+        n= n/2;
     }
-    med = med/i;
-    printf("%d", med);
+    if (n==0) (i=0);
+    return i;
+}
+
+
+int main () {
+    int n;
+    int i;
+    scanf("%d",&n);
+    printf("%d", bitsUm(n,i));
+
     return 0;
 }
